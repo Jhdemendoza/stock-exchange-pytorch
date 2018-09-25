085dbe3856754399d959625c4b1423fa00d8ac7b
Mike Baik <baik.mike@gmail.com>
2018-09-25 02:16:14 -0400

    Adding num_state_per_ticker variable

--- gym_exchange/envs/stock_exchange.py
+++ gym_exchange/envs/stock_exchange.py
@@ -9,13 +9,13 @@ class StockExchange(gym.Env):
     metadata = {'render.modes': ['human']}
 
     # Keep tickers in a list or iterables...
-    tickers = ['aapl', 'amd', 'msft']
+    tickers = ['aapl', 'amd', 'msft', 'intc', 'd', 'sbux', 'atvi', 'ibm', 'ual', 'vrsn', 't', 'mcd', 'vz']
     start_date = '2014-01-01'
     num_days_to_iterate = 1000
     num_state_space = 20
     # if Portfolio, set it to length of tickers
     # else, must be odd
-    num_action_space = 3
+    num_action_space = len(tickers)
     # no_action_index is truly no_action only if it's not a Portfolio
     no_action_index = num_action_space//2
     today = 0
@@ -23,6 +23,8 @@ class StockExchange(gym.Env):
     # set to None when not using Portfolio
     action_space_min = 0.0
     action_space_max = 1.0
+    # For each ticker state: ohlc
+    num_state_per_ticker = 4
 
     def __init__(self, seed=None):
 
@@ -47,7 +49,7 @@ class StockExchange(gym.Env):
                               num_action_space=self.num_action_space, render=self.render)
 
         self.action_space = spaces.Discrete(self.num_action_space)
-        self.observation_space = spaces.Box(-1.0, 10.0, (self.num_state_space, self.ticker_length), dtype=np.float)
+        self.observation_space = spaces.Box(-1.0, 2.0, (self.num_state_space, self.ticker_length), dtype=np.float)
         self.state = self.get_running_state()
         self.reset()
 
@@ -81,20 +83,25 @@ class StockExchange(gym.Env):
         return repr(self.env)
 
     def get_running_state(self):
-        return np.zeros((self.num_state_space, self.ticker_length))# .tolist()
+        return np.zeros((self.num_state_space, self.num_state_per_ticker * self.ticker_length))
 
     def add_new_state(self, new_states_to_add):
         assert isinstance(new_states_to_add, list), type(new_states_to_add)
         # Disregarding the last elem in each state because it's the holdings...
-        # Maybe just get rid of that all together?
-        new_states = np.array([state[:-1].tolist() for state in new_states_to_add])
+        # Maybe just get rid of that altogether?
+        new_states = np.array([state[:-1].tolist() for state in new_states_to_add]).flatten()
+
         running_state_orig = self.state
         running_state = pd.DataFrame(running_state_orig).shift(-1)
+
         # Assign new price to index == last_elem - 1
         running_state.iloc[-1] = new_states.squeeze()
+
+        # Deprecated...
         # running_state.iloc[-2] = new_state_to_add.item(0)
         # Assign new position to index == last_elem
         # running_state.iloc[-1] = new_state_to_add.item(1)
+
         assert len(running_state_orig) == len(running_state)
         return np.array(running_state)
 
